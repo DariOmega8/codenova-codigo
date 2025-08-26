0b77752a4b9ff1c1b12360006ab88db1c6a989b6
DariOmega8 <dariomarquez814@gmail.com>
2025-08-26 17:05:21 -0300

    proceso de cambios de codigo

--- reservas.php
+++ reservas.php
@@ -4,27 +4,29 @@ include 'conexion.php';
 
 if (isset($_POST['confirmar'])) {
 
-    if ( empty($_POST['personas']) || empty($_POST['hora']) ||empty($_POST['fecha']) ||empty($_POST['telefono'])  ) {
+    if ( empty($_POST['personas']) || empty($_POST['hora']) ||empty($_POST['fecha']) ) {
           echo "error en los datos"; 
     } else {
 
         $cantidad = $_POST['personas'];
         $hora = $_POST['hora'];
         $fecha = $_POST['fecha'];
-        $telefono = $_POST['telefono'];
 
         $sql = "INSERT INTO reserva (hora, cantidad, fecha ) VALUES ('$hora', '$cantidad', '$fecha')";
 
-        $sql2 = "INSERT INTO reserva telefono (telefono) VALUES ('$telefono')";
-        
         $resultado = mysqli_query($conexion, $sql);
-
-        if ($resultado) {
+        
+            if ($resultado) {
             header("Location: reservas.html");
             exit;
+
         } else {
             echo "Error en la reserva: " . mysqli_error($conexion);
         }
+
+        }
+        
+        
     }
 }
 
